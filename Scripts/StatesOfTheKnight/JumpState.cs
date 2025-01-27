1b58598cb400469d042da307dc110993ff19ddab
btwcerf16 <btwcerf14506@gmail.com>
2025-01-27 22:46:34 +0300

    Initial commit

---
+++ Scripts/StatesOfTheKnight/JumpState.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpState : State
+{
+    private Player _player;
+    public JumpState(Player player)
+    {
+        _player = player;
+    }
+
+    
+    public override void Enter()
+    {
+        base.Enter();
+        _player.animator.SetTrigger("Jump");
+        _player.rigidbody2D.AddForce(_player.jumpForce * Vector2.down, ForceMode2D.Impulse);
+        
+    }
+
+
+    public override void Update()
+    {
+        base.Update();
+        _player.transform.position = new Vector2((_player.transform.position.x + _player.speed * Time.deltaTime * _player.moveVector.x)/3, _player.transform.position.y);
+
+        if (_player.moveVector.x > 0 && !_player.faceRight || _player.moveVector.x < 0 && _player.faceRight)
+        {
+            _player.Flip();
+        }
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        
+    }
+}
